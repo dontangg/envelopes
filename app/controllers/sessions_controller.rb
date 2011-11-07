ae0aefb09afc230e458e13f8e8ebe56237cbc1fe
Don Wilson <robert.don.wilson@gmail.com>
2011-11-07 08:00:13 -0700

    Styled the login page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_url, :notice => "Logged in!"
+      redirect_to root_url, notice: "Logged in!"
     else
       flash.now.alert = "Invalid email or password"
       render "new"
@@ -15,6 +15,6 @@ class SessionsController < ApplicationController
   
   def destroy
     session[:user_id] = nil
-    redirect_to login_url, :notice => "Logged out!"
+    redirect_to login_url, notice: "Logged out!"
   end
 end
