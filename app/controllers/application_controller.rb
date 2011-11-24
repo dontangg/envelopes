89ef599097ab543121658f5d3aeadd4c4fc6bade
Don Wilson <robert.don.wilson@gmail.com>
2011-11-23 23:24:55 -0700

    Require login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_filter :authenticate
   protect_from_forgery
   
   private
+  
+  def authenticate
+    unless current_user
+      flash[:return_to] = request.fullpath
+      redirect_to login_url
+    end
+  end
   
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
