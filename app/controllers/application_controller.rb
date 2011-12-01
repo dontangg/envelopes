68c1b8b8dc6b2a6c476f93a069d995f5322e82d4
Don Wilson <robert.don.wilson@gmail.com>
2011-12-01 01:17:00 -0700

    Improved main envelopes view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,5 +15,9 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   
+  def current_user_id
+    session[:user_id]
+  end
+  
   helper_method :current_user
 end
--- app/controllers/envelopes_controller.rb
+++ app/controllers/envelopes_controller.rb
@@ -1,5 +1,13 @@
 class EnvelopesController < ApplicationController
   def index
+    all_envelopes = Envelope.owned_by(current_user_id).all_with_amounts
+    
+    @parent_envelopes = []
+    @child_envelopes = Hash.new { |hash, key| hash[key] = [] }
+    all_envelopes.each do |envelope|
+      envelope_array = envelope.parent_envelope_id ? @child_envelopes[envelope.parent_envelope_id] : @parent_envelopes
+      envelope_array.push(envelope)
+    end
   end
 
   def show
