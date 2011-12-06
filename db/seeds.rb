38030a0bfff4f9d8c8607a4f4e3fccbe89edc029
Don Wilson <robert.don.wilson@gmail.com>
2011-12-06 02:49:30 -0700

    Show the transactions of an envelope on Envelope#show

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ when "development"
 
   puts "-- creating users"
 
-  test_user = User.create email: 'test@gmail.com', password: BCrypt::Password.create('pass')
+  test_user = User.create email: 'test@gmail.com', password: 'pass'
 
 
   puts "-- creating envelopes"
@@ -26,10 +26,14 @@ when "development"
   life_insurance_envelope = Envelope.create name: "Life Insurance", user: test_user, parent_envelope: bills_envelope
   mortgage_envelope = Envelope.create name: "Mortgage", user: test_user, parent_envelope: bills_envelope
   credit_card_envelope = Envelope.create name: "Credit Card", user: test_user, parent_envelope: bills_envelope
+  
+  entertainment_envelope = Envelope.create name: 'Entertainment', user: test_user
+  apple_envelope = Envelope.create name: "Apple", user: test_user, parent_envelope: entertainment_envelope
+  other_entertainment_envelope = Envelope.create name: "Other", user: test_user, parent_envelope: entertainment_envelope
 
   food_envelope = Envelope.create name: 'Food', user: test_user
   eating_out_envelope = Envelope.create name: "Eating Out", user: test_user, parent_envelope: food_envelope
-  groceries_maintenance_envelope = Envelope.create name: "Groceries", user: test_user, parent_envelope: food_envelope
+  groceries_envelope = Envelope.create name: "Groceries", user: test_user, parent_envelope: food_envelope
 
   gifts_envelope = Envelope.create name: 'Gifts & Donations', user: test_user
   birthdays_envelope = Envelope.create name: 'Birthdays', user: test_user, parent_envelope: gifts_envelope
@@ -46,4 +50,10 @@ when "development"
   subscriptions_envelope = Envelope.create name: 'Subscriptions', user: test_user
   domains_envelope = Envelope.create name: 'Domains', user: test_user, parent_envelope: subscriptions_envelope
   netflix_envelope = Envelope.create name: 'Netflix', user: test_user, parent_envelope: subscriptions_envelope
+  
+  
+  puts "-- creating transactions"
+  Transaction.create payee: "Wal-Mart", original_payee: '20397235 WALMART AS2385 SPRINGVILLE, UT', posted_at: Date.today - 5.days, amount: -25.64, envelope: groceries_envelope
+  Transaction.create payee: "Target", original_payee: '20397235 TARGET AS2385 OREM, UT', posted_at: Date.today - 4.days, amount: -39.29, envelope: groceries_envelope
+  
 end
