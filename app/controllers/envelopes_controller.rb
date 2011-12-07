1a0c7ac60604658ac266304af94965747f6b16de
Don Wilson <robert.don.wilson@gmail.com>
2011-12-06 17:05:33 -0700

    Added Envelope#full_name and inclusive_total_amount

--- app/controllers/envelopes_controller.rb
+++ app/controllers/envelopes_controller.rb
@@ -6,11 +6,16 @@ class EnvelopesController < ApplicationController
   end
 
   def show
-    all_envelopes = Envelope.owned_by(current_user_id).with_amounts
+    # An array of all envelopes, used by transaction partial to get full envelope name
+    @all_envelopes = Envelope.owned_by(current_user_id).with_amounts
+
+    # The envelope the user is currently looking at
+    @envelope = @all_envelopes.select { |envelope| envelope.id == params[:id].to_i }.first
+
+    # A Hash with all the envelopes organized by parent_envelope_id
+    @envelopes = Envelope.organize(@all_envelopes)
     
-    @envelopes = Envelope.organize(all_envelopes)
-    @envelope = all_envelopes.select { |envelope| envelope.id == params[:id].to_i }.first
-    
+    # An array of transactions in this envelope
     @transactions = @envelope.transactions.recent
   end
 
