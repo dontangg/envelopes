6c92e914447af9712ce4ee8f077cc52de2ff8bc7
Don Wilson <robert.don.wilson@gmail.com>
2011-12-17 15:40:32 -0700

    Added tests for rules

--- test/unit/rule_test.rb
+++ test/unit/rule_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class RuleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "search text and user id are required" do
+    rule = Rule.new
+    assert !rule.save
+    
+    rule.user_id = users(:jim).id
+    assert !rule.save
+    rule.user_id = nil
+    
+    rule.search_text = 'test'
+    assert !rule.save
+    
+    rule.user_id = users(:jim).id
+    assert rule.save
+  end
 end
