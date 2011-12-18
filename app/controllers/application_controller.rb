cf1554b8f7dbd0b9079be23311317f85b0135cf1
Don Wilson <robert.don.wilson@gmail.com>
2011-12-17 20:48:24 -0700

    Tweaks to CanCan setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
   rescue_from CanCan::AccessDenied do |exception|
+    # render :file => "#{Rails.root}/public/403.html", :status => 403
     raise ActionController::RoutingError, exception.message
   end
   
