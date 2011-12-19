9163cbf75d3a267d2a82dceb6de50e0f6d4415df
Don Wilson <robert.don.wilson@gmail.com>
2011-12-19 07:09:56 -0700

    Finish user account page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,15 @@ class UsersController < ApplicationController
   def update
     @user = params[:id] == current_user_id ? current_user : User.find(params[:id])
     authorize! :update, @user
+
+    if params[:question]
+      @user.bank_secret_questions = {}
+      params[:question].each do |index, question|
+        @user.bank_secret_questions[question] = params[:answer][index]
+      end
+    else
+      @user.bank_secret_questions = nil
+    end
     
     if @user.update_attributes(params[:user])
       redirect_to edit_user_url(@user), notice: 'Your account was successfully updated.'
