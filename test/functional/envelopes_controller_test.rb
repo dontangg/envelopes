49144b1dab229a6ee80e316256f9a554db42a7d1
Don Wilson <robert.don.wilson@gmail.com>
2012-01-24 07:02:57 -0700

    Added/fixed some envelopes controller tests

--- test/functional/envelopes_controller_test.rb
+++ test/functional/envelopes_controller_test.rb
@@ -29,15 +29,31 @@ class EnvelopesControllerTest < ActionController::TestCase
     end
   end
 
-  test "should get show" do
+  test "should show an envelope" do
     get :show, id: @envelope.to_param
     assert_response :success
-    assert_equal Date.today - 1.month, assigns(:start_date)
-    assert_equal Date.today, assigns(:end_date)
-    assert_not_nil assigns(:envelope)
-    assert_not_nil assigns(:transactions)
+
     assert_not_nil assigns(:all_envelopes)
     assert_not_nil assigns(:organized_envelopes)
+    assert_not_nil assigns(:envelope)
+    assert_not_nil assigns(:envelope_options_for_select)
+    assert_equal Date.today - 1.month, assigns(:start_date)
+    assert_equal Date.today, assigns(:end_date)
+    assert_not_nil assigns(:show_transfers)
+    assert_not_nil assigns(:transactions)
+  end
+
+  test "should show envelopes to fill" do
+    get :fill
+    assert_response :success
+
+    assert_not_nil assigns(:organized_envelopes)
+    assert_not_nil assigns(:available_cash_envelope)
+  end
+
+  test "should fill the envelopes with money" do
+    post :perform_fill
+    assert_response :success
   end
 
 end
