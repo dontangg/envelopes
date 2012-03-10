60dc897b01785e6771f0f0c148b6331b21e298b4
Don Wilson <robert.don.wilson@gmail.com>
2012-03-10 16:38:45 -0700

    Change bank password hash when user's email changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,17 +6,23 @@ class User < ActiveRecord::Base
   validates_uniqueness_of :email, :on => :create
   
   serialize :bank_secret_questions
-  
+
+  def email=(new_email)
+    pass = self.bank_password
+    super
+    self.bank_password = pass
+  end
+ 
   def bank_password
     unless self.bank_password_cipher.blank?
-      cipher = Gibberish::AES.new(self.password_digest + 's')
+      cipher = Gibberish::AES.new(self.email + 's')
       cipher.dec(self.bank_password_cipher)
     end
   end
   
   def bank_password=(unencrypted_password)
     unless unencrypted_password.blank?
-      cipher = Gibberish::AES.new(self.password_digest + 's')
+      cipher = Gibberish::AES.new(self.email + 's')
       self.bank_password_cipher = cipher.enc(unencrypted_password)
     end
   end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -8,4 +8,14 @@ class UserTest < ActiveSupport::TestCase
     u = User.new(password: 'pass')
     assert !u.save, "Saved the user without an email"
   end
+
+  test "if user changes their email address, the bank password is re-encrypted" do
+    u = User.new(email: 'test', bank_password: 'mypass')
+    
+    assert_equal 'mypass', u.bank_password
+
+    u.email = 'newtest'
+
+    assert_equal 'mypass', u.bank_password
+  end
 end
