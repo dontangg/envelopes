832810811025033182587a5eb2afa5016a40f4a9
Don Wilson <robert.don.wilson@gmail.com>
2012-03-11 23:38:39 -0600

    Allow users to import transactions from their bank

--- app/models/rule.rb
+++ app/models/rule.rb
@@ -7,4 +7,12 @@ class Rule < ActiveRecord::Base
   belongs_to :user
   belongs_to :envelope
 
+  def run(payee)
+    if payee && payee.downcase.include?(self.search_text.downcase)
+      payee = self.replacement_text if self.replacement_text.present?
+      envelope_id = self.envelope_id unless self.envelope_id.nil?
+      [payee, envelope_id]
+    end
+  end
+
 end
