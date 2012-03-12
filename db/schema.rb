6b2ab4d9c1f5e3a48dca3f1aae2b59e9b6d856ed
Don Wilson <robert.don.wilson@gmail.com>
2012-03-11 20:50:06 -0600

    Add bank_account_id column to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120311225448) do
+ActiveRecord::Schema.define(:version => 20120312022505) do
 
   create_table "envelopes", :force => true do |t|
     t.string   "name"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(:version => 20120311225448) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "imported_transactions_at"
+    t.string   "bank_account_id"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
