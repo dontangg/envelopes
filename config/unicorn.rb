ac59e263742d2e1cc6fe84c6dffac50ab2c82411
Don Wilson <robert.don.wilson@gmail.com>
2014-01-04 12:57:28 -0700

    Comment out the zero-downtime deployment code

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -39,15 +39,15 @@ before_fork do |server, worker|
   # we send it a QUIT.
   #
   # This enables 0 downtime deploys.
-  old_pid = "#{pid}.oldbin"
+  #old_pid = "#{pid}.oldbin"
   #old_pid = "#{server.config[:pid]}.oldbin"
-  if File.exists?(old_pid) && server.pid != old_pid
-    begin
-      Process.kill("QUIT", File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH
-      # someone else did our job for us
-    end
-  end
+  #if File.exists?(old_pid) && server.pid != old_pid
+  #  begin
+  #    Process.kill("QUIT", File.read(old_pid).to_i)
+  #  rescue Errno::ENOENT, Errno::ESRCH
+  #    # someone else did our job for us
+  #  end
+  #end
 end
 
 after_fork do |server, worker|
