0e247c4af012ac72f7bf7aa8660f493d86e2b63f
Don Wilson <robert.don.wilson@gmail.com>
2014-12-17 22:45:07 -0700

    Show/hide envelope categories on click

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,13 +4,13 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
 //
-// require jquery
+//= require jquery
 // require jquery.masonry
 // require jquery-ui-1.8.16.custom.min
 // require jquery.select-to-autocomplete
 // require jquery.select-on-focus
 // require jquery_ujs
-// require bootstrap.min
+//= require bootstrap.min
 // require hierarchy-plot
 // require main
 // require_tree .
