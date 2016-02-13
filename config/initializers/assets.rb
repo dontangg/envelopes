254c066505b26ad6c523b72f70fd276b75c8c339
Don Wilson <robert.don.wilson@gmail.com>
2016-02-13 11:47:11 -0700

    Fix missing fonts

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,11 +6,16 @@ Rails.application.config.assets.version = '1.0'
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'stylesheets', 'images')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w(
+  glyphicons-halflings-regular.eot
+  glyphicons-halflings-regular.svg
+  glyphicons-halflings-regular.ttf
+  glyphicons-halflings-regular.woff
   ui-bg_flat_0_aaaaaa_40x100.png
   ui-bg_flat_75_ffffff_40x100.png
   ui-bg_glass_55_fbf9ee_1x400.png
